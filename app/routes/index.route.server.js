97c2eb6053771551ff5c868f3b0710bf5e905f93
YouMadBro!? <amaynar4@my.centennialcollege.ca>
2022-10-01 13:23:44 -0500

    Added comments to various files

--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -1,3 +1,10 @@
+//File: index.route.server.js
+//Student Name: Alexander Maynard
+//Student ID: 301170707
+//Date: 2022-10-01
+
+
+//imports routes defined in index.contoller.server.js
 import { Router } from "express";
 import { displayAbout, 
     displayContact, 
@@ -5,8 +12,12 @@ import { displayAbout,
     displayProjects, 
     displayServices } from "../controllers/index.controller.server.js";
 
+//Instantiate a const instance of router
 const router = Router();
 
+
+//get for the routes. 
+//router.get(browser relative ex: path('/path'), page to get). 
 router.get('/', displayHome);
 router.get('/home', displayHome);
 router.get('/about', displayAbout);
@@ -14,4 +25,5 @@ router.get('/projects', displayProjects);
 router.get('/services', displayServices);
 router.get('/contact', displayContact);
 
+//export to be used by other modules
 export default router;
